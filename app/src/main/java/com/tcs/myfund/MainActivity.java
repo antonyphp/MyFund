7c014c7162c821b5a5905488dfcb3a9b2d96fb15
Antony <antonyphp@gmail.com>
2016-09-27 21:39:35 +0530

    Added below tab layout and fragments

--- app/src/main/java/com/tcs/myfund/MainActivity.java
+++ app/src/main/java/com/tcs/myfund/MainActivity.java
@@ -1,30 +1,44 @@
 package com.tcs.myfund;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ScrollView;
 
 import com.tcs.myfund.adapter.GraphPagerAdapter;
+import com.tcs.myfund.adapter.ViewPagerAdapter;
+import com.tcs.myfund.fragments.AllFundFragment;
+import com.tcs.myfund.fragments.FondskontoFragment;
 import com.tcs.myfund.fragments.LineGraphFragment;
 import com.tcs.myfund.fragments.MapChartFragment;
 import com.tcs.myfund.fragments.PieChartFragment;
+import com.tcs.myfund.fragments.SpareavtalerFragment;
+import com.tcs.myfund.fragments.TransaksjonerFragment;
 import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    String[] mobileArray = {"DNB Aktiv 100","DNB Norge Index","DNB Miloinvest","DNB Aktiv 100","DNB Norge Index","DNB Miloinvest"};
+public class MainActivity extends AppCompatActivity implements AllFundFragment.OnFragmentInteractionListener,FondskontoFragment.OnFragmentInteractionListener,SpareavtalerFragment.OnFragmentInteractionListener,TransaksjonerFragment.OnFragmentInteractionListener{
+//    String[] mobileArray = {"DNB Aktiv 100","DNB Norge Index","DNB Miloinvest","DNB Aktiv 100","DNB Norge Index","DNB Miloinvest"};
     FragmentPagerAdapter adapterViewPager;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +55,23 @@ public class MainActivity extends AppCompatActivity {
         CirclePageIndicator titleIndicator = (CirclePageIndicator) findViewById(R.id.circle_page_indicator);
         titleIndicator.setViewPager(vpPager);
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        LinearLayout linearLayout = (LinearLayout)tabLayout.getChildAt(0);
+        linearLayout.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        drawable.setSize(1, 1);
+        linearLayout.setDividerDrawable(drawable);
+
+//        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mobileArray);
+//
+//        ListView listView = (ListView) findViewById(R.id.list_view);
+//        listView.setAdapter(adapter);
 //        listView.setOnTouchListener(new View.OnTouchListener() {
 //            // Setting on Touch Listener for handling the touch inside ScrollView
 //            @Override
@@ -54,8 +81,17 @@ public class MainActivity extends AppCompatActivity {
 //                return false;
 //            }
 //        });
-        setListViewHeightBasedOnChildren(listView);
-        ((ScrollView)findViewById(R.id.full_scroll_view)).smoothScrollTo(0,0);
+//        setListViewHeightBasedOnChildren(listView);
+//        ((ScrollView)findViewById(R.id.full_scroll_view)).smoothScrollTo(0,0);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AllFundFragment(), "Alle Fond");
+        adapter.addFragment(new FondskontoFragment(), "Fondskonto");
+        adapter.addFragment(new SpareavtalerFragment(), "Spareavtaler");
+        adapter.addFragment(new TransaksjonerFragment(), "Transaksjoner");
+        viewPager.setAdapter(adapter);
     }
 
 
@@ -101,5 +137,10 @@ public class MainActivity extends AppCompatActivity {
         fList.add(new MapChartFragment());
         fList.add(new PieChartFragment());
         return fList;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
